6d666d9c54174cfd72624a5b4f01c574c0f8cd77
Nasr-edine <d.nasredine@gmail.com>
2024-08-06 16:46:56 +0200

    Add programs to print numbers 1 to 10 using for loop and while loop for question 13

---
+++ chap_2_flow_control/question_13_print_numbers.py
@@ -0,0 +1,17 @@
+# question_13_print_numbers.py
+
+# Question 13:
+# Write a short program that prints the numbers 1 to 10 using a for loop.
+# Then write an equivalent program that prints the numbers 1 to 10 using a while loop.
+
+# For Loop Example
+print("Using for loop:")
+for i in range(1, 11):
+    print(i)
+
+# While Loop Example
+print("\nUsing while loop:")
+i = 1
+while i <= 10:
+    print(i)
+    i += 1
